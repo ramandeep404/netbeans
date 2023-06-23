4ccadfd45958f8ef1cff7f9134d99adfd41339db
Raman Deep Singh <raman404@hotmail.com>
2023-06-23 19:56:39 +0200

    mmbbmbmbm

--- NodeJsApplication/main.js
+++ NodeJsApplication/main.js
@@ -2,5 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
  */
-console.log('jkj');
-console.log('dsfd');
+const Sequelize = require('sequelize');
+
+const path = 'mysql://root:@localhost:3306/nodejsdb1';
+const sequelize = new Sequelize(path, {
+    operatorsAliases: false
+});
+
+let Dummy = sequelize.define('students13', {
+    description: Sequelize.STRING
+});
+
+Dummy.sync().then(() => {
+    console.log('New table created');
+}).finally(() => {
+    sequelize.close();
+});
